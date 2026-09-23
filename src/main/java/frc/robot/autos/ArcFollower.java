6280d5812221617c22d7b375bdb94426ec72d16f
jaimieissupercoolfrfr <jaimie.weyman@gmail.com>
2026-09-23 11:46:17 -0700

    9/23 auto point

--- src/main/java/frc/robot/autos/ArcFollower.java
+++ src/main/java/frc/robot/autos/ArcFollower.java
@@ -3,7 +3,6 @@ package frc.robot.autos;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.robot_manager.RobotManager;
 import frc.robot.util.Circle;
 import frc.robot.util.FmsUtil;
